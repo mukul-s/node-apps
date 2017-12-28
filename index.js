4ed046027725534b3634d27c34cce905f1094d1c
mukul571995 <mukul571995@gmail.com>
2017-12-29 01:41:25 +0530

    Node http module example1

---
+++ index.js
@@ -0,0 +1,22 @@
+// importing http core module
+const http = require('http');
+
+// some other required variable
+const hostname = "localhost";
+
+// default port
+const port = '3000';
+
+// creating server 
+const server = http.createServer((res, req) => {
+    console.log(req.header);
+
+    req.statusCode = 200
+    req.setHeader('Content-type', 'text/html');
+    req.end('<html><body>Rock on, hain ye waqt ka ishara</body></html>');
+})
+
+// start the server
+server.listen(post, hostname, () => {
+    console.log(`Our Url is https://${localhost}/${port}`);
+});
